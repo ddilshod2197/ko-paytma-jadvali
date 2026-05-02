1e13571247923d41ebc387f3b92aa50f4000ed91
Dilshod <ddilshod2197@gmail.com>
2026-05-02 16:56:30 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,17 @@
+def birlashtir_ro'yxatlar(ro'yxat1, ro'yxat2):
+    return list(set(ro'yxat1 + ro'yxat2))
+
+ro'yxat1 = [1, 2, 2, 3, 4, 5]
+ro'yxat2 = [4, 5, 6, 7, 8]
+
+print(birlashtir_ro'yxatlar(ro'yxat1, ro'yxat2))
+```
+
+```python
+def birlashtir_ro'yxatlar(ro'yxat1, ro'yxat2):
+    return list(set(ro'yxat1 + ro'yxat2))
+
+ro'yxat1 = ['a', 'b', 'b', 'c', 'd', 'e']
+ro'yxat2 = ['e', 'f', 'g', 'h', 'i']
+
+print(birlashtir_ro'yxatlar(ro'yxat1, ro'yxat2))
